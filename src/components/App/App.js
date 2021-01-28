3cba6688b1e69159ca75c384c7728516d1d970ac
vzm5 <73701054+vzm5@users.noreply.github.com>
2021-01-28 13:22:56 -0500

    Build SearchBar, BusinessList, Business and App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,15 @@
+import React from 'react';
+import './App.css';
 import logo from '../../logo.svg';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h1>ravenous</h1>
+      <SearchBar/>
     </div>
   );
 }
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './Business.css';
 
-var business {
+var business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
     address: '1010 Paddington Way',
@@ -12,7 +12,7 @@ var business {
     category: 'Italian',
     rating: 4.5,
     reviewCount: 90
-}
+};
 
 class Business extends React.Component {
     render(){
