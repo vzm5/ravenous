a138b463a986d3c14f959b24abb68e9790419609
vzm5 <73701054+vzm5@users.noreply.github.com>
2021-01-28 14:12:21 -0500

    Finish part 2 of project: passing information between components.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,38 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div class="App">
-      <h1>ravenous</h1>
-      <SearchBar/>
-    </div>
-  );
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar/>
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,13 +5,13 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render() {
-        return (<div className="BusinessList">
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
+        return (
+        <div className="BusinessList">
+            {
+            this.props.businesses.map(business => {
+                return <Business business={business}/>
+            }) 
+            }
         </div>);
     }
 }
